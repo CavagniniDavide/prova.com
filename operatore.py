d24916a58a2bf8e94649ed9cce962d70abacb038
Davide.Cavagnini <cavagninidavide@gmail.com>
2020-11-19 14:00:38 +0100

    tornato alla versione originale

--- operatore.py
+++ operatore.py
@@ -32,7 +32,6 @@ while True:
     scelta=input("""
     Azioni possibili:
     areaq ->se vuoi calcolare l'area del quadrato
-    arear ->se vuoi calcolare l'area di un rettangolo
     """)
     if scelta=="areaq":
         lato=int(input("Inserisci il lato del quadrato:"))
